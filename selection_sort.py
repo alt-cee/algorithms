d5003714b541d46b884abc4ad0500f6020f37ae4
ai-lien <a.trancong@gmail.com>
2023-05-04 14:25:28 -0700

    add selection sort docstrings and clean up

--- selection_sort.py
+++ selection_sort.py
@@ -1,8 +1,14 @@
 def find_smallest(a: list) -> int:
     """
     Finds the index of the smallest value in the array.
+
+    Args:
+        a (list): The array 'a' to search for the smallest value.
+
+    Returns:
+        int: The index of the smallest value in the array 'a'.
     """
-    min = a[0] # need to keep track of the min value
+    min = a[0]
     index = 0
     for i in range(1, len(a)):
         if a[i] < min:
@@ -10,16 +16,28 @@ def find_smallest(a: list) -> int:
             index = i
     return index
 
+
 def selection_sort(a: list) -> list:
     """
-    Sorts the items in the array 'a' in order from smallest to largest.
+    Sorts a list of comparable items in the array 'a' in order from smallest 
+    to largest using the selection sort algorithm.
+
+    Selection sort works by iteratively finding the minimum of the input array 
+    'a' and adding it to the end of a new list. 
+
+    Args:
+        a (list): An array of items to sort.
+
+    Returns:
+        list: A sorted array.
     """
     sorted_a = []
     for i in range(len(a)):
         index_smallest = find_smallest(a)
-        smallest = a.pop(index_smallest) # deleting from an array, modifying the input array
-        sorted_a.append(smallest)  # inserting into an array
+        smallest = a.pop(index_smallest)
+        sorted_a.append(smallest)
     return sorted_a
+
 
 if __name__ == "__main__":
     print(selection_sort([-1, -5, 7, 2, 9]))
