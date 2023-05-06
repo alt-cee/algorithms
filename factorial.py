3a035739a377680132d174a8c2740af6721ee674
ai-lien <a.trancong@gmail.com>
2023-05-06 14:29:11 -0700

    add factorial calculated using recursion

--- factorial.py
+++ factorial.py
@@ -1,10 +1,18 @@
-def factorial(x: int) -> int:
+def loop_factorial(x: int) -> int:
     answer = 1
     for v in range(1, x + 1):
         answer *= v
     return answer
 
 
+def recursive_factorial(x: int) -> int:
+    if x == 1:
+        return 1
+    else:
+        return x * recursive_factorial(x - 1)
+
+
 if __name__ == "__main__":
     x = int(input("Enter an integer: "))
-    print(factorial(x))
+    print(loop_factorial(x))
+    print(recursive_factorial(x))
