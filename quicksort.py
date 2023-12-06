7aefd86f9d80be774760e7a6e8c533d6fddcf92f
ai-lien <a.trancong@gmail.com>
2023-12-06 11:49:41 -0800

    rewrite quickSort and add inplace quickSort

--- quicksort.py
+++ quicksort.py
@@ -1,22 +1,54 @@
-def quicksort(array: list):
+def quickSort1(array: list):
+
     if len(array) < 2:
         return array
-    # elif len(array) == 2:
-    #     if array[0] < array[1]:
-    #         return array
-    #     else:
-    #         return [array[1], array[0]]
     else:
-        pivot = array.pop()
-        left = []
-        right = []
-        for i in array:
-            if i >= pivot:
-                right.append(i)
-            else:
-                left.append(i)
-        return quicksort(left) + [pivot] + quicksort(right)
+        pivot = array[0]
+        left = [x for x in array[1:] if x <= pivot]
+        right = [x for x in array[1:] if x > pivot]
+        return quickSort1(left) + [pivot] + quickSort1(right)
+    
+
+def swap(array: list, i, j):
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+    return None
+
+
+def partition(array:list, l:int, h: int):
+    """
+    Sorts values into less than and greater than the pivot value in place and
+    returns the sorted location of the pivot (first element) in the array.
+    """
+    if l == h:
+        return l
+    
+    else:
+        pivot = array[l]
+        i = l + 1
+        j = h
+
+        while i < j:
+            while array[i] <= pivot:
+                i += 1
+            while array[j] > pivot:
+                j -= 1
+            swap(array, i, j)
+            i += 1
+            j -= 1
+            array.insert(j, array.pop(l))
+        
+        return j
+
+
+def quickSort2(array:list, l:int, h: int):
+    
+    
 
 if __name__ == "__main__":
-    array = [100, 2, 1, 870, 0, 3, -5]
-    print(quicksort(array))
+    array = [10, 5, 2, 6, 4, 11, 1]
+    print("array: ", array)
+    # print("quickSort1: ", quickSort1(array))
+    print("partition: ", partition(array, 0, len(array) - 1))
+    # print("quickSort2: ", quickSort2(array))
