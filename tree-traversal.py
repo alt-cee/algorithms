8423113d8ff5739576441ef7e7465352abe24d23
ai-lien <a.trancong@gmail.com>
2024-01-03 11:13:20 -0800

    add recursive preorder traversal

---
+++ tree-traversal.py
@@ -0,0 +1,31 @@
+import queue
+
+class Node:
+    
+    def __init__(self, value = None, left = None, right = None) -> None:
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def create_tree():
+    root = Node("A")
+    root.left = Node("B")
+    root.right = Node("C")
+    root.left.left = Node("D")
+    root.left.right = Node("E")
+    root.right.left = Node("F")
+    root.right.right = Node("G")
+    return root
+
+def recursive_preorder(node):
+    if not node:
+        return None
+    else:
+        print(node.value)
+        recursive_preorder(node.left)
+        recursive_preorder(node.right)
+
+if __name__ == "__main__":
+    root = create_tree()
+    recursive_preorder(root)
